f827137781e6a0cbb183874fb16fc5bb471ba482
lilyshowlader <lilyshowlader@gmail.com>
2022-12-04 14:31:23 -0500

    setTerm and handleChange function added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,20 @@
+import { useState } from "react"
 function SearchBar ({onSubmit} ) {
-
+  const [term, setTerm] = useState('')
   const handleFormSubmit = (event) => {
     event.preventDefault()
     // console.log('I need to tell the parent about some data')
     onSubmit('cats')
   }
   // to detect user pressing enter key, we are wrapping the input with a form element. by doing this, the browser triggers a submit event 
+
+  const handleChange = (event) => {
+    setTerm(event.target.value)
+  }
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-      <input />
+      <input value={term} onChange= {handleChange}/>
       </form>
     </div>
   )

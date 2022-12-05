b33475a0a173c5df2dbe769237372c19b4e4e755
lilyshowlader <lilyshowlader@gmail.com>
2022-12-05 12:37:55 -0500

    set image state, passed prop to ImageList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import { useState } from 'react'
 import SearchBar from './components/SearchBar'
 import searchImages from './services/api-calls'
+import ImageList from './components/ImageList'
 function App () {
+  const [images, setImages] = useState([])
 
   // create handleSubmit that takes in term (term is coming from the child component)
 
-  const handleSubmit = (term) => {
-    searchImages(term)
-  
+  const handleSubmit = async (term) => {
+   const result = await searchImages(term)
+    // result is going to be an array of objects - this is the API response
+    // after doing a search, we want to update content on the screen with the new list of images.
+    setImages(result)
   }
 
 
   return (
     <>
     <SearchBar onSubmit={handleSubmit}/>
+    <ImageList images={images}/>
     </>
   )
 }
